6d6c2452e0e7a56a6a53cba4292bda7c066ff03b
njc-econ <nathanjcarroll.econ@gmail.com>
2021-03-05 23:48:32 +0100

    shopping list editor

---
+++ shoppinglist.php
@@ -0,0 +1,32 @@
+<?php
+  
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Groceries made Easy: Shopping List</title>
+  </head>
+  <body>
+
+    <h1>Your shopping list</h1>
+    <table>
+      <tr>
+        <form method="post">
+        <td>
+          <input type="text" name="newItem" value="Item" id="newItem" size="50px">
+         </td>
+         <td>
+           <input type="text" name="newQuantity" value="1" id="newQuantity" size= "10px">
+          </td>
+          <td>
+            <input type="submit" name="itemSubmit" value="Add" id="newQuantity" size= "20px">
+           </td>
+        </form>
+      </tr>
+    </table>
+  </body>
+</html>
