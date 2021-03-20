643f891170a7021048f075d9ccee041083ff1892
njc-econ <nathanjcarroll.econ@gmail.com>
2021-03-20 10:41:01 +0100

    Add logout option to start screen

--- start.php
+++ start.php
@@ -1,8 +1,9 @@
 <?php
   session_start();
 
-  if (!isset($_SESSION['name'])){
+  if (!isset($_SESSION['user_id'])){
     header('Location: index.php');
+    require 'logout.php';
   }
   if ( isset($_POST['logout']) ) {
       session_destroy();
@@ -20,7 +21,7 @@
    </head>
    <body>
     <h1>Welcome <?php echo $_SESSION['name']?>!</h1>
-
+    <?php if (isset($_SESSION['error'])){echo $_SESSION['error']; unset($_SESSION['error']);} ?>
     <h2>Your recipes</h2>
 
 
@@ -28,5 +29,9 @@
 
 
     <h2><a href="shoppinglist.php">Your Shopping List</a></h2>
+
+    <h2><a href="recipeinput.php">Create New Recipe</a></h2>
+
+    <a href="logout.php">Logout</a>
    </body>
  </html>
