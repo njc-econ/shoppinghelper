64ccbaabb5e2ca162a8d64f4221993839a814362
njc-econ <nathanjcarroll.econ@gmail.com>
2021-04-02 16:09:00 +0200

    Use CDN for jquery

--- headerscript.php
+++ headerscript.php
@@ -1,3 +1,10 @@
+<!--
 <link rel="stylesheet" href="jquery-ui.css">
-<script src="jquery-3.6.0.min.js"></script>
-<script src="jquery-ui.js"></script>
+-->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+			  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+			  crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"
+			  integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30="
+			  crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/shStyle.css">
