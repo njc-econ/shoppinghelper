8383a380635e6786db5010ad306dd85202a8a057
njc-econ <nathanjcarroll.econ@gmail.com>
2021-04-02 16:10:27 +0200

    language and image tables

--- tableSetup.sql
+++ tableSetup.sql
@@ -8,9 +8,16 @@ CREATE TABLE `languages` (
   INDEX (`lang_long`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
-INSERT INTO languages (lang_short, lang_long) VALUES ('de','German');
-INSERT INTO languages (lang_short, lang_long) VALUES ('en','English');
-INSERT INTO languages (lang_short, lang_long) VALUES ('es','Spanish');
+INSERT INTO languages (lang_short, lang_long) VALUES ('de','German'),('en','English'),('es','Spanish');
+
+CREATE TABLE `recipeType` (
+  recipeType_id INT(2) unsigned NOT NULL AUTO_INCREMENT,
+  recipeType_nameEng VARCHAR(30) UNIQUE,
+  PRIMARY KEY (`recipeType_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+
+INSERT INTO recipeType (recipeType_nameEng) VALUES ('Starter'),('Main meal'),('Dessert'),('Snack'),('Other');
+
 
 CREATE TABLE `users` (
   `user_id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -27,6 +34,7 @@ CREATE TABLE `users` (
 
 CREATE TABLE recipeHead (
   `recipe_id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+  `recipeType_id` INT(2) unsigned,
   `title` VARCHAR(255) NOT NULL,
   `vegetarian` INT(1) NOT NULL,
   `vegan` INT(1) NOT NULL,
@@ -38,9 +46,10 @@ CREATE TABLE recipeHead (
   `user_id` INT(11) unsigned,
   `lang_id` INT(2) unsigned,
   PRIMARY KEY (`recipe_id`),
+  CONSTRAINT FOREIGN KEY (recipeType_id) REFERENCES recipeType (recipeType_id),
   CONSTRAINT FOREIGN KEY (user_id) REFERENCES users (user_id),
   CONSTRAINT FOREIGN KEY (fork_id) REFERENCES recipeHead (recipe_id),
-  CONSTRAINT FOREIGN KEY (`lang`) REFERENCES languages (lang_id)
+  CONSTRAINT FOREIGN KEY (`lang_id`) REFERENCES languages (lang_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
 CREATE TABLE ingredients (
@@ -52,14 +61,29 @@ CREATE TABLE ingredients (
   UNIQUE KEY (`name`,`lang_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
+CREATE TABLE images (
+  `image_id` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+  `recipe_id` INT(11) unsigned NOT NULL,
+  `filename` VARCHAR(30) NOT NULL UNIQUE,
+  `image_rank` INT(1), /* in future allow multiple images per recipe, rank 0 is the header image, subsequent numbers correspond to steps */
+  `upload_date` DATETIME,
+  PRIMARY KEY (`image_id`),
+  CONSTRAINT FOREIGN KEY (`recipe_id`) REFERENCES recipeHead (`recipe_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+
+
 CREATE TABLE recipeIngredients (
   `recipe_id` INT(11) unsigned NOT NULL,
   `ingredient_id` INT(11) unsigned NOT NULL,
   `quantity` FLOAT,
   `measure` VARCHAR(5),
   `input_rank` INT(2) unsigned NOT NULL,
-  CONSTRAINT FOREIGN KEY (`recipe_id`) REFERENCES recipeHead (`recipe_id`),
-  CONSTRAINT FOREIGN KEY (`ingredient_id`) REFERENCES ingredients (`ingredient_id`),
+  CONSTRAINT FOREIGN KEY (`recipe_id`)
+    REFERENCES recipeHead (`recipe_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT FOREIGN KEY (`ingredient_id`)
+    REFERENCES ingredients (`ingredient_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
   PRIMARY KEY (`recipe_id`, `ingredient_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
