7a7c65b768c53cf5e8fcb35ab471c9f2e56428a5
njc-econ <nathanjcarroll.econ@gmail.com>
2021-04-15 16:19:38 +0200

    provide list of recipe ingredients on shopping (summarized)

---
+++ shoppinglistrecipes.php
@@ -0,0 +1,29 @@
+<?php
+  require_once 'pdo.php';
+  session_start();
+
+  $stmt = $pdo -> prepare('
+  SELECT
+  ingredientShopping.item_id,
+  SUM(recipeIngredients.quantity),
+  recipeIngredients.measure,
+  ingredients.name
+  FROM recipeShopping
+  JOIN recipeIngredients
+  ON recipeShopping.sourcerecipe_id = recipeIngredients.recipe_id
+  JOIN ingredientShopping
+  ON recipeIngredients.ingredient_id=ingredientShopping.ingredient_id
+  JOIN ingredients
+  ON recipeIngredients.ingredient_id = ingredients.ingredient_id
+  WHERE user_id = :user_id
+  GROUP BY ingredientShopping.item_id, recipeIngredients.measure, ingredients.name
+  ');
+
+  $result = $stmt -> execute(array(
+    ':user_id' => $_SESSION['user_id']
+  ));
+  $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+  $output = json_encode($result);
+  header('Content-Type: application/json; charset=utf-8');
+  echo($output);
+?>
