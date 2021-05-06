0e2e89c699df78ebd5e02b61ed46c1e71502698e
njc-econ <nathanjcarroll.econ@gmail.com>
2021-05-06 17:29:22 +0200

    collect list of recipes on user shopping list

---
+++ recipeonlist.php
@@ -0,0 +1,24 @@
+<?php
+  require_once 'pdo.php';
+  session_start();
+
+  $stmt = $pdo -> prepare('
+    SELECT
+      sourcerecipe_id AS recipe_id
+      ,title
+      ,numserved
+    FROM recipeShopping
+    INNER JOIN recipeHead
+    ON recipeShopping.sourcerecipe_id = recipeHead.recipe_id
+    WHERE recipeShopping.user_id = :user_id'
+  );
+
+  $result = $stmt -> execute(array(
+    ':user_id' => $_SESSION['user_id']
+  ));
+  $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+  $output = json_encode($result);
+  header('Content-Type: application/json; charset=utf-8');
+  echo($output);
+
+?>
